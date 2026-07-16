9dabd1c8f75562ab430f8c9cad6b0346a74c224e
Vara693 <pvaradraj2@gmail.com>
2026-07-16 18:15:06 +0530

    Added find-pivot-index.cpp

---
+++ find-pivot-index.cpp
@@ -0,0 +1,24 @@
+// Given an array of integers nums, calculate the pivot index of this array.
+
+// The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+
+// If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
+
+// Return the leftmost pivot index. If no such index exists, return -1.
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int total = 0;
+        for (int x : nums) total += x;
+
+        int leftSum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            int rightSum = total - leftSum - nums[i];
+            if (leftSum == rightSum) return i;
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+};
